2f34b77fb7e0edf26bdc2d53979f134c38cc75e2
zhangchenze <zhangchenze@gomeholdings.com>
2018-03-27 13:09:54 +0800

    first init commit

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-import { AppRegistry } from 'react-native';
-import IosApp from './index.ios';
-import AndirApp from './index.android';
-
-AppRegistry.registerComponent('rockzcReactnative', () => IosApp);
-AppRegistry.registerComponent('rockzcReactnative', () => AndirApp);
+import './lib/index'
